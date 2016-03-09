636bfdd291085dda13d29b4c20f4b2ba28e01fb4
joshcodes <josh@blackbarlabs.com>
2016-03-08 18:47:28 -0600

    init

---
+++ IProvideCredentials.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Security.CredentialProvider
+{
+    public interface IProvideCredentials
+    {
+        /// <summary>
+        /// This method validates that the provided token is valid for
+        /// the specified username and provider.
+        /// </summary>
+        /// <param name="providerId">Identifies the provider to use</param>
+        /// <param name="username">Identifiers the user in the remote system</param>
+        /// <param name="token">Token/password to use to authenticate the user</param>
+        /// <returns>Value which will be stored for future access to this system. The return value must
+        /// not be a default or empty string if the token was valid.</returns>
+        Task<string> RedeemTokenAsync(Uri providerId, string username, string token);
+    }
+}
