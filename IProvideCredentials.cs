ebf927f693d7c9f424ccf6629a408fff8c7677c1
Keith Holloway <keith@eastfive.com>
2016-04-08 11:22:10 -0500

    Added error messaging capabilities for credential checks

--- IProvideCredentials.cs
+++ IProvideCredentials.cs
@@ -12,9 +12,12 @@ namespace BlackBarLabs.Security.CredentialProvider
         /// <param name="providerId">Identifies the provider to use</param>
         /// <param name="username">Identifiers the user in the remote system</param>
         /// <param name="token">Token/password to use to authenticate the user</param>
+        /// <param name="success"></param>
+        /// <param name="invalidCredentials"></param>
+        /// <param name="couldNotConnect"></param>
         /// <returns>Value which will be stored for future access to this system. The return value must
         /// not be a default or empty string if the token was valid.</returns>
         Task<TResult> RedeemTokenAsync<TResult>(Uri providerId, string username, string token,
-            Func<string, TResult> success, Func<TResult> invalidCredentials, Func<TResult> couldNotConnect);
+            Func<string, TResult> success, Func<string, TResult> invalidCredentials, Func<TResult> couldNotConnect);
     }
 }
