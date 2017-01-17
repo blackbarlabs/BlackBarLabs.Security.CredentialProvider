0d2e917a20885b79b5162de24f5500bb34fcdcac
Josh <josh@eastfive.com>
2017-01-16 18:37:38 -0800

    pivoting to LoginProvider

---
+++ IProvideLogin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace EastFive.Security.LoginProvider
+{
+    public interface IProvideLogin
+    {
+        Task<TResult> ValidateToken<TResult>(string idToken,
+            Func<SecurityToken, ClaimsPrincipal, TResult> onSuccess,
+            Func<string, TResult> onFailed);
+
+        Task<TResult> CreateLoginAsync<TResult>(Guid loginId,
+            string userId, bool isEmail, string secret, bool forceChange,
+            Func<Guid, TResult> onSuccess,
+            Func<string, TResult> onFail);
+    }
+}
